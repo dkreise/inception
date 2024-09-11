c5e78ad5e0b63e902364d4582d3486c6829fde63
dkreise <dia.kreis@gmail.com>
2024-09-11 17:27:47 +0200

    added user and a theme to wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -7,6 +7,8 @@ else
 	wp core download --allow-root
 	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
 	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
+	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
+	wp theme install astra --activate --allow-root
 fi
 
 /usr/sbin/php-fpm7.4 -F;
